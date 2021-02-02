6ba2bb2320f4cd629313c2efc37dbbd7a0081c2d
Pugsly99 <rylandadams@yahoo.com>
2021-02-02 13:24:25 -0800

    storeState
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

---
+++ src/js/plantGrowing.js
@@ -0,0 +1,11 @@
+// This function stores state
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+
+  }
+}
